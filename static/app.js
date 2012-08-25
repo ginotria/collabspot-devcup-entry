3ee0a2a5d9352b0738f111eb222c0edae76e9ffd
Gino Tria <gino@wemakeprojects.com>
2012-08-25 15:22:49 +0800

    attachment mvc

--- static/app.js
+++ static/app.js
@@ -1,5 +1,4 @@
 (function($){
-
     var DC = {
         models: {},
         collections: {},
@@ -8,8 +7,57 @@
         }
     };
 
+    DC.models.Class = Capsule.Model.extend({
+        type: 'class',
+        initialize: function() {
+            this.register();
+            this.addChildCollection('attachments', DC.collections.Attachments);
+        }
+    });
+    DC.models.Attachment = Capsule.Model.extend({
+        type: 'attachment',
+        initialize: function() {
+            this.register();
+        }
+    });
+
+    DC.collections.Attachments = Capsule.Collection.extend({
+        type: 'attachments',
+        // Reference to this collection's model.
+        model: DC.models.Attachment,
+        initialize: function() {
+            this.register();
+        }
+    });
+
+    DC.views.AttachmentView = Capsule.View.extend({
+        template: 'attachmentTemplate',
+
+        events: {
+            'click .remove-attachment': 'removeAttachment'
+        },
+
+        initialize: function(spec) {
+            this.containerEl = spec.containerEl;
+            this.render();
+        },
+
+        render: function() {
+            console.log('render attachment', this.containerEl);
+            this.subViewRender();
+        },
+
+        removeAttachment: function() {
+        }
+
+
+    });
+
     DC.views.AppView = Capsule.View.extend({
         gapiCount: 0,
+        events: {
+            'click .new-attachment': 'onAddAttachment'
+        },
         initialize: function() {
             this.loadGapis();
         },
@@ -40,13 +88,17 @@
             this.handleBindings();
             console.log('App Initialization Complete.');
 
-            //test gapi
-            var r = gapi.client.drive.files.list();
-            r.execute(function(resp) {
-                console.log('Google Drive Test', resp);
-            });
+            this.collectomatic(this.model.attachments, DC.views.AttachmentView, {containerEl:this.$('.list-attachments')[0]});
+        },
+
+        onAddAttachment: function() {
+           alert('test');
         }
     });
 
-    app = window.app = new DC.views.AppView();
+    classModel = window.classModel = new DC.models.Class();
+    app = window.app = new DC.views.AppView({
+        model: classModel
+    });
+
 })(jQuery);
