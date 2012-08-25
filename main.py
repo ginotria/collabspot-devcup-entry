d0eea678d7b1320e268a5ae679e7c80b21fb8c90
Gino Tria <gino@wemakeprojects.com>
2012-08-25 18:29:32 +0800

    oauthirazation

--- main.py
+++ main.py
@@ -1,39 +1,82 @@
 from google.appengine.ext import webapp
 
+import httplib2
 import jinja2
 import os
 import cgi
 import datetime
 import urllib
 import webapp2
+import logging
 
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api import memcache
+from google.appengine.ext.webapp.util import run_wsgi_app
+from apiclient.discovery import build
+import httplib2
+from oauth2client.appengine import OAuth2Decorator
+from oauth2client.appengine import OAuth2Handler
+import settings
+
+try:
+    import json  # python 2.6
+except ImportError:
+    import simplejson as json  # python 2.4 to 2.5
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+decorator = OAuth2Decorator(client_id=settings.CLIENT_ID,
+                            client_secret=settings.CLIENT_SECRET,
+                            scope=settings.SCOPE)
 
-class MainHandler(webapp2.RequestHandler):
+
+class MainHandler(webapp.RequestHandler):
+
+    @decorator.oauth_aware
     def get(self):
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
+        if decorator.has_credentials():
+            tmpl = jinja_environment.get_template('templates/index.html')
+            if users.get_current_user():
+                url = users.create_logout_url(self.request.uri)
+                url_linktext = 'Logout'
+            else:
+                url = users.create_login_url(self.request.uri)
+                url_linktext = 'Login'
         else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
+            tmpl = jinja_environment.get_template('templates/grant.html')
+            url = decorator.authorize_url()
 
         template_values = {
-            'logout_url': url,
-            'url_linktext': url_linktext,
+            'url': url,
             'user_email': 'romanangelo.tria@gmail.com'
         }
+        self.response.out.write(tmpl.render(template_values))
 
-        template = jinja_environment.get_template('templates/index.html')
-        self.response.out.write(template.render(template_values))
 
-app = webapp.WSGIApplication([('/', MainHandler)],
-                             debug=True)
+class UploadHandler(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write(json.dumps({
+            "test": "ola"
+        }))
 
+# app = webapp.WSGIApplication([
+#     ('/', MainHandler),
+#     ('/about', MainHandler),
+#     ('/upload', UploadHandler),
+#     (decorator.callback_path, decorator.callback_handler())
+# ], debug=True)
+def truncate(s, l):
+    return s[:l] + '...' if len(s) > l else s
+
+app = webapp.WSGIApplication(
+   [
+        ('/', MainHandler),
+        ('/upload', UploadHandler),
+        ('/oauth2callback', OAuth2Handler)
+   ],
+   debug=True)
+
+if __name__ == '__main__':
+    run_wsgi_app(app)
